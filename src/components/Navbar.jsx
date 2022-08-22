47e15e6c0d903d9f90e6732e96238562cec150ae
Yash Deharia <yashdeharia@Yashs-MacBook-Pro.local>
2022-08-22 22:29:53 +0530

    files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const navbar = () => {
+
+    
+
+  return (
+
+    <div className='flex justify-between items-center w-full h-20 px-4 text-green-400 font-mono font-bold fixed'>
+
+        <div>
+            <h1 className='text-4xl ml-2'>OxygenX</h1>
+            <p className='text-black'>.live</p>
+        </div>
+
+        <ul className='hidden md:flex'>
+            <li className='px-4 cursor-pointer capitalize text-green-400 hover:scale-150 duration-200'>About</li>
+            <li className='px-4 cursor-pointer capitalize text-green-400 hover:scale-150 duration-200'>Green <span className='text-black'>paper</span></li>
+            <li className='px-4 cursor-pointer capitalize text-green-400 hover:scale-150 duration-200'>Buy/sell NFT </li>
+            <li className='px-4 cursor-pointer capitalize text-green-400 hover:scale-150 duration-200'> Contact </li>
+            <li className='px-4 cursor-pointer capitalize text-green-400 hover:scale-150 duration-200'>Oxy-token</li>
+
+
+        </ul>
+
+        
+
+        
+    
+
+    </div>
+
+  )
+}
+
+export default navbar
---
+++ src/components/nft.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const nft = () => {
+  return (
+    <div>
+        <h1>
+            Working on MarketPlace!!!
+        </h1>
+    </div>
+  )
+}
+
+export default nft
