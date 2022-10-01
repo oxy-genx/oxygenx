ba521983750eb846586b15b293e35bd2b1c343ea
Yash Deharia <yashdeharia@Yashs-MacBook-Pro.local>
2022-10-01 18:28:03 +0530

    changed about to black bg

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,15 @@ const About = () => {
                 <h1 className='text-center text-4xl sm:text-5xl font-bold font-mono'>About Us.</h1>
             </div>
 
-            <div className='mt-20 text-center border-y-2 border-black hover:background-white'>
-                <p className='font-bold text-2xl font-mono'>OxygenX is a emerging brand that will save the World without any central needs. <br />
-                    There is no PLANET B. The Planet you live-in is only the Planet to survive. But we are rigorously exploiting the Planet because we don't value it. <br />
+            <div className='mt-20 text-center border-y-2 border-black bg-black'>
+                <p className='text-white font-bold text-2xl font-mono'>OxygenX is a emerging brand that will save the World without any central needs. <br /> 
+                <br />
+                    There is no PLANET B. The Planet you live-in is only the Planet to survive. But we are rigorously exploiting the Planet because we don't value it. 
+                    <br />
+                    <br />
                     Here we, Present you the OxygennX started by Yash Deharia, to solve the biggest REAL WORLD PROBLEM. That will give you the ownership of the nature -- That bring Value to the Mother Nature. <br />
-                    -- Yash Deharia (Founder & CEO)</p>
+                    <br />
+                    -- <span className='text-green-300'>Yash Deharia</span> (Founder & CEO)</p>
             </div>
 
 
