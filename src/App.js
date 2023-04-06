3edb2e060dcedb9ca725ea617ec0107793736554
yashdeharia <67191325+yashdeharia@users.noreply.github.com>
2023-04-06 16:35:01 +0000

    Updated code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Team from "./components/team";
 import Sidebar from "./components/Sidebar";
+import Form from "./components/Form";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div>
@@ -12,6 +15,14 @@ function App() {
       <Home />
       <About />
       <Team />
+      <h1 className=' mt-4 text-center text-4xl sm:text-5xl font-bold font-mono team'>Contact Us</h1>
+      
+        <div className='bg-green flex flex-col justify-center h-full '>
+          <Form />
+        </div>
+
+        <Footer />
+      
     
 
       
